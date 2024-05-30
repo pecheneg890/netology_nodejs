8c537c86da276ac224e9be88241c94c05a008855
olaaveryanov <olaaveryanov@greenatom.ru>
2024-05-30 20:32:00 +0300

    fix max

--- task1/index.js
+++ task1/index.js
@@ -3,10 +3,8 @@ const readline = require('node:readline');
 const { stdin: input, stdout: output } = require('node:process');
 
 const rl = readline.createInterface({ input, output });
-const min = 0;
-const max = 10;
-const hiddenNumber = min + Math.floor(Math.random() * (max + 1 - min));
-console.log(`Загадано число в диапазоне от ${min} до ${max}`);
+const hiddenNumber = Math.floor(Math.random() * (max - min));
+console.log(`Угадай число`);
 
 getNextAnswer();
 
